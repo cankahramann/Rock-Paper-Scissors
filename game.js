8da8e1b5da2417abc831da88db04d8dc9f9f1646
Can Kahraman <candevelops@Xubuntu.myguest.virtualbox.org>
2023-02-24 16:55:38 +0300

    Create function 'getComputerChoice'

---
+++ game.js
@@ -0,0 +1,13 @@
+function getComputerChoice(){
+    let num = Math.floor(Math.random()*3+1)
+    if (num==1) {
+        return "Rock"
+    } else if (num==2){
+        return "Paper"
+    }else{
+        return "Scissors"
+    }
+}
+
+
+console.log(getComputerChoice());
